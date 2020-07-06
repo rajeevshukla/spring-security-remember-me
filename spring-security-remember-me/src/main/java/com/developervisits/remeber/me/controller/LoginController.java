b670a056ce8cf7e26d57dcd6eee09abfed02aeca
rajeevshukla <mail2rajeevshukla@gmail.com>
2020-07-06 15:20:07 -0500

    login page

--- spring-security-remember-me/src/main/java/com/developervisits/remeber/me/controller/LoginController.java
+++ spring-security-remember-me/src/main/java/com/developervisits/remeber/me/controller/LoginController.java
@@ -1,8 +1,16 @@
 package com.developervisits.remeber.me.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "login";
+	}
+	
 }
